cd3dfb04eddd16f124065c2d4d346a93b86a2e79
Lynn-zuo <1528174573@qq.com>
2020-07-07 14:41:38 +0800

    add icon-display

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ var keys = [
     ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 ]
 var hash = {
-    '1': '12306.cn', '2': '163.com', '3': undefined, '4': undefined, '5': undefined,
+    '1': '12306.cn', '2': '163.com', '3': 'stackoverflow.com', '4': undefined, '5': undefined,
     '6': undefined, '7': undefined, '8': undefined, '9': undefined, '0': undefined,
     'q': 'qzone.qq.com', 'w': undefined, 'e': undefined, 'r': undefined, 't': undefined,
     'y': 'youdao.com', 'u': undefined, 'i': undefined, 'o': undefined, 'p': undefined,
     'a': undefined, 's': undefined, 'd': undefined, 'f': undefined, 'g': 'github.com',
     'h': undefined, 'j': undefined, 'k': undefined, 'l': undefined,
     'z': undefined, 'x': undefined, 'c': undefined, 'v': undefined, 'b': 'baidu.com', 
-    'n': 'nowcoder.com','m': 'meituan.com',
+    'n': 'nowcoder.com','m': 'meituan.com'
+}
+var imgroute ={
+    '12306.cn': './imgs/12306.png', '163.com': './imgs/163.jpg', 'baidu.com': './imgs/baidu.png',
+    'qzone.qq.com': './imgs/qzone.jpg', 'youdao.com': './imgs/youdao.png', 'github.com': './imgs/github.png',
+    'nowcoder.com': './imgs/nowcoder.png', 'meituan.com': './imgs/meituan.jpg', 'stackoverflow.com': './imgs/stackoverflow.png'
 }
 // 取出localStorage中的update_website对应的hash
 hashInLocalStorage = JSON.parse(localStorage.getItem('update_website') || 'null')
@@ -30,7 +35,6 @@ while(i < keys.length){
     while(j < keys[i].length){
         kbdX = document.createElement('kbd');
         buttonXE = document.createElement('button');
-        //buttonXD = document.createElement('button');
         kbdX.textContent = keys[i][j];
         kbdX.id = keys[i][j];
         kbdX.onclick = function(keyb){
@@ -43,20 +47,25 @@ while(i < keys.length){
           window.open('http://' +  website, '_blank');
         }
         buttonXE.textContent = 'E';
-        //buttonXD.textContent = 'D';
         buttonXE.id = keys[i][j];
         buttonXE.onclick = function(keyd){
             // keyd.target是用户点击的元素
             console.log(keyd.target.id)
             keyX = keyd.target.id
-            new_website = prompt('给我一个网址')
+            new_website = prompt('更改按键对应的导航网址')
             hash[keyX] = new_website // hash变更
             localStorage.setItem('update_website', JSON.stringify(hash));
-            console.log(hash)
+            // console.log(hash)
         }
         divX.appendChild(kbdX);
         kbdX.appendChild(buttonXE);
-        //kbdX.appendChild(buttonXD);
+        websi = hash[kbdX.id];
+        if(websi!==undefined){
+            imgX = document.createElement('img');
+            imgX.src = imgroute[websi];
+            // console.log(websi,imgX.src)
+            kbdX.appendChild(imgX);
+        }
         j+=1
     }
     i+=1;
